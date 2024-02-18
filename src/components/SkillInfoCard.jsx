a39187b80fd2f424d50f37ae04367916369d5335
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-02-17 21:55:04 -0500

    Update skill info header to be darker.

--- src/components/SkillInfoCard.jsx
+++ src/components/SkillInfoCard.jsx
@@ -3,7 +3,9 @@ import "./SkillInfoCard.css";
 
 const SkillInfoCard = ({ heading, skills }) => (
   <div className="skill-info-card">
-    <h6>{heading}</h6>
+    <div className="skill-info-header">
+      <h6>{heading}</h6>
+    </div>
 
     <div className="skill-info-content">
       {skills.map((item, index) => (
