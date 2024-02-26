75b682f8ac8580f80e02adf36cdba2db8a699868
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-02-26 17:48:02 -0500

    Remove NavBarMobile CSS.

--- src/components/navbar/NavBarMobile.jsx
+++ src/components/navbar/NavBarMobile.jsx
@@ -1,17 +1,21 @@
 import "./NavBarMobile.css";
-import { TITLE, NAV_LINKS, BUTTON_TEXT } from "../../utils/data";
+import { NAV_LINKS, BUTTON_TEXT } from "../../utils/data";
 
 const NavBarMobile = ({ isOpen, toggleMenu }) => (
+  // mobile-menu
   <div className={`mobile-menu ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
+    {/* mobile-menu-container */}
     <div className="mobile-menu-container">
       <ul>
         {NAV_LINKS.map((nav, index) => (
           <li key={nav.id}>
+            {/* menu-item */}
             <a className="menu-item" href={`#${nav.id}`}>
               <h2>{nav.title}</h2>
             </a>
           </li>
         ))}
+        {/* contact-btn */}
         <a className="contact-btn" href="#contact">
           <h3>{BUTTON_TEXT}</h3>
         </a>
