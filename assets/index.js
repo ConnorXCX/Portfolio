8650894fa05a682b2be39b596a622b3c38010eaf
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-02-28 00:36:06 -0500

    Add GitHub Pages dependencies.

---
+++ assets/index.js
@@ -0,0 +1,33 @@
+import githubLogo from "./contacts/github-logo.svg";
+import linkedInLogo from "./contacts/linkedin.png";
+import phoneLogo from "./contacts/phone.png";
+import emailLogo from "./contacts/email.png";
+import awsDeveloperAssociateLogo from "./certifications/AWS_Certified_Developer_Associate.png";
+import awsCloudPractitionerLogo from "./certifications/AWS_Certified_Cloud_Practitioner.png";
+import gearsIcon from "./skills/gears.png";
+import frontendIcon from "./skills/front-end.png";
+import backendIcon from "./skills/back-end.png";
+import devopsIcon from "./skills/devops.png";
+import cloudIcon from "./skills/cloud.png";
+import agileIcon from "./skills/sprint.png";
+import softSkillsIcon from "./skills/soft_skills.png";
+import unicorn from "./index/unicorn.png";
+import unicorn2 from "./index/unicorn2.png";
+
+export {
+  githubLogo,
+  linkedInLogo,
+  phoneLogo,
+  emailLogo,
+  awsDeveloperAssociateLogo,
+  awsCloudPractitionerLogo,
+  gearsIcon,
+  frontendIcon,
+  backendIcon,
+  devopsIcon,
+  cloudIcon,
+  agileIcon,
+  softSkillsIcon,
+  unicorn,
+  unicorn2,
+};
--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => (
     <NavBar />
     <div className="max-w-[1300px] relative mx-auto my-0 xl:px-6 xl:py-0">
       <Hero />
-      <Skills />
+      {/* <Skills /> */}
       <Certifications />
       {/* <Projects /> */}
       {/* <Books /> */}
