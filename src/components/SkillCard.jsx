6fc4ec9835ea3f8220cfa5cc54b7c64d113c9985
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-02-28 21:16:08 -0500

    Minor refactor.

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,8 +1,10 @@
 const SkillCard = ({ title, iconUrl, invertImage, isActive, onClick }) => (
   <div
     className={`${
-      isActive ? "bg-defaultDarkerBlurBackground text-secondary" : ""
-    } bg-defaultLightBlurBackground text-center backdrop-blur-lg relative cursor-pointer transition-all duration-[0.3s] ease-[ease] p-10 rounded-[0.65rem] border-[1.5px] border-solid border-primary hover:bg-defaultDarkBlurBackground`}
+      isActive
+        ? "bg-defaultDarkerBlurBackground text-secondary"
+        : "bg-defaultLightBlurBackground"
+    } text-center backdrop-blur-lg relative cursor-pointer transition-all duration-[0.3s] ease-[ease] p-10 rounded-[0.65rem] border-[1.5px] border-solid border-primary hover:text-secondary hover:bg-defaultDarkBlurBackground`}
     onClick={() => onClick()}
   >
     <div className="bg-primary w-16 h-16 flex items-center justify-center absolute top-[-1rem] left-[-1rem] rounded-[0.65rem] border-[1.5px] border-solid border-primary">
